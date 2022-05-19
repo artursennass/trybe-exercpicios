0df87b5b18da74db112938e5416fc9a66d049104
Artur Senna <artursennass@gmail.com>
2022-05-19 19:00:39 -0300

    práticas da aula feitos

---
+++ fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia1-dom-e-seletores/pratica-2/script.js
@@ -0,0 +1,15 @@
+document.querySelector('header').style.backgroundColor = 'green'
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink'
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'purple'
+
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple'
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow'
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black'
+
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black'
+
+document.querySelector('footer').style.backgroundColor = 'black'
