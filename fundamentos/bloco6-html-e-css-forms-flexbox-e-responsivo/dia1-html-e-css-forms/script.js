00d0bd790e024af0f560c4876b159ec565eb6b66
Artur Senna <artursennass@gmail.com>
2022-06-01 16:25:41 -0300

    consegui fazer o exercício 4 mas nao consegui fazer o 3 ainda

--- fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/script.js
+++ fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/script.js
@@ -1,6 +1,20 @@
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault()
+
 let sendButton = document.getElementById(send-button);
 
 sendButton.addEventListener('click', function(event){
     event.preventDefault();
 });
 
+
+
+// Crie um botão que limpe as informações contidas nos campos;
+let deleteContent = document.getElementById('delete-content');
+
+deleteContent.addEventListener('click', function(event){
+    let input = document.getElementsByTagName('input');
+
+    for (let i = 0; i < input.length; i += 1) {
+        input[i].innerHTML = ""
+    }
+});
