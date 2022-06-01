cb75a5968e688aa6967a25fc2f22d6de32ed1a64
Artur Senna <artursennass@gmail.com>
2022-06-01 19:16:06 -0300

    terminei os exercícios, não consegui resolver o ultimo bonus

--- fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/script.js
+++ fundamentos/bloco6-html-e-css-forms-flexbox-e-responsivo/dia1-html-e-css-forms/script.js
@@ -1,6 +1,6 @@
 // Interrompa o comportamento padrão do botão submit utilizando o método preventDefault()
 
-let sendButton = document.getElementById(send-button);
+const sendButton = document.getElementById('send-button');
 
 sendButton.addEventListener('click', function(event){
     event.preventDefault();
@@ -9,12 +9,52 @@ sendButton.addEventListener('click', function(event){
 
 
 // Crie um botão que limpe as informações contidas nos campos;
-let deleteContent = document.getElementById('delete-content');
+// const deleteContent = document.getElementById('delete-content');
 
-deleteContent.addEventListener('click', function(event){
-    let input = document.getElementsByTagName('input');
+// deleteContent.addEventListener('click', function(event){
+//     let input = document.getElementsByTagName('input');
 
-    for (let i = 0; i < input.length; i += 1) {
-        input[i].innerHTML = ""
+//     for (let i = 0; i < input.length; i += 1) {
+//         input[i].innerHTML = ""
+//     }
+// });
+
+//melhor usar o type reset no botão
+
+const hiddenDiv = document.getElementById('hidden');
+
+function imagesAuthorized() {
+    let inputImage = document.createElement('input');
+    inputImage.setAttribute('type', 'file');
+
+    hiddenDiv.appendChild(inputImage);
+}
+
+function removeFileUpload() {
+    hiddenDiv.removeChild(hiddenDiv.firstElementChild);
+}
+
+
+const imageAgrement = document.getElementById('image-agrement');
+
+imageAgrement.addEventListener('click', function(event){
+    if (imageAgrement.checked === true) {
+        imagesAuthorized();
+    } else {
+        removeFileUpload();
     }
-});
+})
+
+
+const contestantName = document.getElementById('contestant-name');
+const contestantEmail = document.getElementById('contestant-email');
+
+function dataAssessment() {
+    if (contestantEmail.innerText < 10 || contestantName.innerText < 10) {
+        alert('Dados Inválidos')
+    } else {
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+    }
+}
+
+sendButton.addEventListener('click', dataAssessment)
